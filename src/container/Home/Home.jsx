2b3b6d6c588841bfbc6bb3d43b58bf6f05b4b164
kunc <kurookun@gmail.com>
2019-12-16 11:50:21 +0700

    [*] first commit

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,38 +1,43 @@
-import React, {Component} from 'react';
-//import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-import Product from '../Product/Product';
+//libraries
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+//pages
+import Product from '../pages/Product/Product';
+import LifeCycleComp from '../pages/LifeCycleComp/LifeCycleComp';
+import BlogPost from '../pages/BlogPost/BlogPost';
+import YoutubeCompPage from '../pages/YoutubeCompPage/YoutubeCompPage';
+import DetailPost from '../pages/BlogPost/DetailPost/DetailPost'
+
+//style
+import './Home.css';
 
 class Home extends Component{
+    state = {
+        showComponent: true
+    }
+
     render(){
         return (
-        <div>
-            {/* <p>YouTube Component</p>
-            <hr/>
-            <YouTubeComp 
-                time="7.12" 
-                title="Tutorial 1"
-                desc="Sudah ditotonton 1x"/>
-             <YouTubeComp 
-                time="8.02" 
-                title="Tutorial 2"
-                desc="Sudah ditotonton 12x"/>
-            <YouTubeComp 
-                time="5.04" 
-                title="Tutorial 3"
-                desc="Sudah ditotonton 123x"/>
-            <YouTubeComp 
-                time="4.12" 
-                title="Tutorial 4"
-                desc="Sudah ditotonton 1234x"/>
-            <YouTubeComp /> */}
-            <p>Counter</p>
-            <hr/>
-            <Product />
-        </div>
+            <Router>
+                <Fragment> 
+                    <div className="navigation">
+                        <Link to="/">Blog Post</Link>
+                        <Link to="/product">Product</Link>
+                        <Link to="/lifecycle">LifeCycle</Link>
+                        <Link to="/youtube-component">YouTube</Link>
+                    </div>
+                    <Route path="/" exact component={BlogPost} />
+                    <Route path="/detail-post/:PostId" component={DetailPost} />
+                    <Route path="/product" component={Product} />
+                    <Route path="/lifecycle" component={LifeCycleComp} />
+                    <Route path="/youtube-component" component={YoutubeCompPage} />
+                </Fragment>
+            </Router>
+
         )
     }
-    
+
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Home from './container/Home/Home';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const globalState = {
+    totalOrder: 0
+}
+
+// Reducer
+const rootReducer = (state = globalState, action) => {
+    if(action.type === 'PLUS_ORDER'){
+        return {
+            ...state,
+            totalOrder: state.totalOrder + 1
+        }
+    }
+    if (action.type === 'MINUS_ORDER') {
+        let totalOrder = 0;
+        if(state.totalOrder >0) {
+            totalOrder = state.totalOrder - 1
+        }
+        return {
+            ...state,
+            totalOrder: totalOrder
+        }
+        
+    }
+
+    return state;
+}
+
+// Store
+const storeRedux = createStore(rootReducer);
 
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={storeRedux}> <Home /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
